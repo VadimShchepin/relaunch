ec1fe5d26c3158d221724cc4956b453845c59784
VadimShchepin <vadimshch21@gmail.com>
2026-01-19 10:27:54 +0100

    feat: add umami tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
                 <link href="https://api.fontshare.com/v2/css?f[]=general-sans@400,500,600,700&display=swap" rel="stylesheet" />
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="theme-color" content="#4FAF8C" />
+                <script defer src="https://umami.dsgvoschulfotos.de/script.js" data-website-id="994bf95f-79f8-4fcd-b77f-48abe6baee1d"></script>
             </head>
             <body className="antialiased">
                 {children}
